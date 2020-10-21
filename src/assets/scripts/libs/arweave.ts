d7fcc62b4f868116497df5eb21b84f9760f9ead7
Cedrik <cedrik.git@tryninja.io>
2020-10-21 10:14:03 -0500

    🎨arweave.net and claim requirement

--- src/assets/scripts/libs/arweave.ts
+++ src/assets/scripts/libs/arweave.ts
@@ -2,7 +2,7 @@ import Arweave from 'arweave';
 
 // @ts-ignore
 export default (window.arweave = Arweave.init({
-  host: 'arweave.dev',
+  host: 'arweave.net',
   protocol: 'https',
   port: 443,
   timeout: 100000
