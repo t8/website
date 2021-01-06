89be8780ea158e289825fe65ed527470b3eea6be
Cedrik <cedrik.git@tryninja.io>
2021-01-05 21:29:15 -0600

    👽sentry

--- src/assets/scripts/global.ts
+++ src/assets/scripts/global.ts
@@ -2,13 +2,21 @@
  * This one is used on all parts of the admin panel.
  */
 
-import * as Sentry from '@sentry/browser';
-import { Integrations } from '@sentry/tracing';
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
 
 Sentry.init({
-  dsn: 'https://797cc582ba794c93934d01bf44722e96@o440904.ingest.sentry.io/5410617',
+  dsn: "https://797cc582ba794c93934d01bf44722e96@o440904.ingest.sentry.io/5410617",
+
+  // To set your release version
+  release: "community-website@" + process.env.npm_package_version,
   integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
+
 });
 
 import * as feather from 'feather-icons';
